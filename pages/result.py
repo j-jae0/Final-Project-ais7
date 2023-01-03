5aa726390c0ad86399b6313f5e0c5977fb072b16
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-01-04 02:19:04 +0900

    Update result.py

--- pages/result.py
+++ pages/result.py
@@ -1,7 +1,5 @@
-from nturl2path import url2pathname
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
 import koreanize_matplotlib
