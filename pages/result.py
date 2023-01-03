bc243f9a2a88a2fae1ad1b42f5847750e755ebb5
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-01-04 01:36:34 +0900

    Create result.py

---
+++ pages/result.py
@@ -0,0 +1,7 @@
+from nturl2path import url2pathname
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import koreanize_matplotlib
