f612811d5893937589ea6a81e5194989e0b4aeab
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-01-03 17:38:10 +0900

    Update webtoon.py

--- webtoon.py
+++ webtoon.py
@@ -5,25 +5,13 @@ import seaborn as sns
 # 폰트 적용 
 with open( "style.css" ) as css:
     st.markdown(f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+
+# 데이터 로드
+LINK_URL = "https://raw.githubusercontent.com/j-jae0/Final-Project-ais7/main/data/%E1%84%8B%E1%85%B0%E1%86%B8%E1%84%90%E1%85%AE%E1%86%AB_%E1%84%89%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%85%E1%85%B5%E1%86%BC%E1%84%8F%E1%85%B3.csv"
+df_link = pd.read_csv(LINK_URL)
+
     
 "# 작전명: 띵작을 찾아서"
-
-def main() :
-    df_link = pd.read_csv("https://raw.githubusercontent.com/j-jae0/Final-Project-ais7/main/data/%E1%84%8B%E1%85%B0%E1%86%B8%E1%84%90%E1%85%AE%E1%86%AB_%E1%84%89%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%85%E1%85%B5%E1%86%BC%E1%84%8F%E1%85%B3.csv")
-
-if __name__ == '__main__' :
-    main()
-    
-# @st.cache
-# def webtoon_info(URL):
-#     df = pd.read_csv(URL)
-#     return df
-
-# URL = "data/웹툰_섬네일_링크.csv"
-# df_link = webtoon_info(URL)
-
-# df_link = pd.read_csv("/app/final-project-ais7/data/웹툰_섬네일_링크.csv")
-# df_link.loc[df_link["title"]=="뱀파이어의 연금술", ["id", "title", "thumbnail"]]
 
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
