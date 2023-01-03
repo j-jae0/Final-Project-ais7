ffc84e2a74213cc662d667f8dd5ee21303e3acf0
Jaeyoung <114332270+j-jae0@users.noreply.github.com>
2023-01-03 17:36:48 +0900

    Update webtoon.py

--- webtoon.py
+++ webtoon.py
@@ -9,7 +9,7 @@ with open( "style.css" ) as css:
 "# 작전명: 띵작을 찾아서"
 
 def main() :
-    df_link = pd.read_csv("data/웹툰_섬네일_링크.csv")
+    df_link = pd.read_csv("https://raw.githubusercontent.com/j-jae0/Final-Project-ais7/main/data/%E1%84%8B%E1%85%B0%E1%86%B8%E1%84%90%E1%85%AE%E1%86%AB_%E1%84%89%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF_%E1%84%85%E1%85%B5%E1%86%BC%E1%84%8F%E1%85%B3.csv")
 
 if __name__ == '__main__' :
     main()
